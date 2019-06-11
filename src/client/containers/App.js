c6e060ddef0aeee6a39f10f4b084cae7fdcec12b
eloi duwer <eduwer@student.42.fr>
2019-06-11 22:52:50 +0200

    debut

---
+++ src/client/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = (props) => {
+	return (
+		<div>
+			a que coucou!
+		</div>
+	);
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+	entry: './src/client/index.js',
+
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js',
+	},
+
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			options: {
+				presets: ["@babel/react"],
+			}
+		}]
+	}
+};
